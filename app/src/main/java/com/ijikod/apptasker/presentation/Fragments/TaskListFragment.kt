c1f6c9e907cd4dfd43a8905f036aa7f574d0bc79
samdan <samdan@ijikod.com>
2021-02-19 18:55:58 +0000

    WIP | implementing presentation layer

--- app/src/main/java/com/ijikod/apptasker/presentation/Fragments/TaskListFragment.kt
+++ app/src/main/java/com/ijikod/apptasker/presentation/Fragments/TaskListFragment.kt
@@ -12,22 +12,17 @@ import com.ijikod.apptasker.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MainFragment : Fragment() {
+
+class TaskListFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_task_list, container, false)
     }
 
 }
